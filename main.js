123c9c5dabb28af4812e98c46270507dd91f9dc5
IntPDFX-PINTUCO <127631195+IntPDFX-PINTUCO@users.noreply.github.com>
2024-02-17 16:17:40 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-//https://teachablemachine.withgoogle.com/models/TDYVIVOLE/
-Webcam.set({width:600, height:480})
+ia=ml5.imageClassifier("model.json",listo)
+Webcam.set({width:640, height:480})
 Webcam.attach("#camara")
 function tomarfoto() {
     console.log("Tomando Foto");
@@ -8,4 +8,24 @@ function tomarfoto() {
         document.getElementById("camara").style.display="none"
         document.getElementById("analizar").style.display="block"
     })
+}
+function analizar() {
+    ia.classify(document.getElementById("foto"),resultado)
+}
+function listo() {
+ console.log("modelo ok");
+}
+function resultado(error,respuesta) {
+ if (error) {
+    console.log(error)
+ } else {
+    console.log(respuesta)
+    for (let objeto = 0; objeto < respuesta.length; objeto++) {
+        label=respuesta[objeto].label
+        confidence=respuesta[objeto].confidence
+        confidence=Math.round(confidence*100)
+        document.getElementById("resultado").innerHTML+=label+" "+confidence+"%<br>"
+        
+    }
+ }
 }
